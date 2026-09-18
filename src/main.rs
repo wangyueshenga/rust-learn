0a7874fabf7014f3d1e5eeb47a2fa5e9f9df0616
wys <2719464332@qq.com>
2026-09-19 05:38:11 +0800

    bernoulli 测试完成

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,8 @@
 // #[allow(unused_imports)]
 // use std::thread;
 // use thread_do::cpu_work;
-
-use rand::distr::Bernoulli;
-use rand::prelude::*;
-use rand::distr;
-/* thoudsands:: Separable 是一个 trait,它提供了 separate_with_underscores() 方法,可以把数字格式化为带下划线的形式,提高可读性。*/
-use thousands::Separable;
+mod bernoulli_get;
+use bernoulli_get::*;
 // use rand::distr::Distribution;
 #[allow(unused)]
 #[allow(unused_imports)]
@@ -28,65 +24,14 @@ use rand::rngs::*;
 #[allow(unused)]
 #[allow(unused_imports)]
 use rand::seq::*;
-#[doc = 
-    r#"
-    1. Bernoulli 分布是一个离散概率分布,它只有两个可能的结果:成功(通常表示为 1)和失败(通常表示为 0)。成功的概率为 p,失败的概率为 1-p。
-    2. 在 Rust 中,可以使用 rand crate 提供的 distr::Bernoulli 来生成 Bernoulli 分布的随机样本。
-    3. 使用 distr::Bernoulli::new(p) 创建一个 Bernoulli 分布实例,其中 p 是成功的概率。
-    4. 然后可以使用 sample 方法从该分布中采样,返回一个 bool 值,表示成功或失败。
-    5. 可以通过多次采样来估计成功的概率,并与理论值进行比较。
-    "#
-    ]
-    #[allow(unused)]
-fn test_bernoulli(){
-  let mut count = 0;
-  let mut count2 = 0;
-  let mut count3 = 0;
-  let bernoulli = distr::Bernoulli::new(0.3).unwrap();
-  for _ in 0..1_000_000_000{
-    count2 += 1;
-    if count2 % 1_000_000 == 0{
-      count3 += 1;
-      println!("已完成 {} * 1_000_000 次实验", count3.separate_with_underscores());
-      count2 = 0;
-      
-    }
-    let value: bool = bernoulli.sample(&mut rand::rng());
-    if value{
-      count += 1;
-    }
-  }
-  println!("\n在 1_000_000_000 次实验中,成功概率为 {} %", count as f64 / 1_000_000_000.0);
-}
-
-#[
-  doc = r#"
-   测试 Bernoulli::p() 方法,它返回成功的概率 p。
-
-  "#
-]
-#[allow(unused)]
-fn test_bernoulli_fn_p(){
-  // 测试 Bernoulli::p() 方法,它返回成功的概率 p。
-  let temp = Bernoulli::new(0.3).unwrap();
-  let value = temp.p();
-  println!( "值为{:.3} ",value);
-}
-
-#[allow(unused)]
-fn test_bernoulli_fn_from_ratio(){
-  // 测试 Bernoulli::from_ratio() 方法,它接受两个整数参数,分别表示成功和失败的次数,并返回一个 Bernoulli 分布实例。
-  let temp = Bernoulli::from_ratio(3, 7).unwrap();
-  let value = temp.p();
-  println!( "值为{:.3} ",value);
-}
 
 fn main() {
 
+  test_bernoulli_sample();
 
   test_bernoulli_fn_from_ratio();
   // test_bernoulli();
-  // test_bernoulli_fn_p();
+  test_bernoulli_fn_p();
   // let mut rng = rand::rng();
   // let num: Vec<u64> = (0..10).map(|_| rng.random_range(0..101)).collect();
 
